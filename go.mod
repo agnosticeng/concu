28a38d7b29985a3e4cb6de91cb8a986b78c857a5
Arnaud Briche <briche.arnaud@gmail.com>
2025-01-08 14:25:43 +0100

    chore: add size-limited memo backed by theine-go

--- go.mod
+++ go.mod
@@ -2,4 +2,13 @@ module github.com/agnosticeng/concu
 
 go 1.23.2
 
-require golang.org/x/sync v0.10.0
+require (
+	github.com/Yiling-J/theine-go v0.6.0
+	golang.org/x/sync v0.10.0
+)
+
+require (
+	github.com/klauspost/cpuid/v2 v2.2.9 // indirect
+	github.com/zeebo/xxh3 v1.0.2 // indirect
+	golang.org/x/sys v0.29.0 // indirect
+)
--- memo/memo.go
+++ memo/memo.go
@@ -1,6 +1,10 @@
 package memo
 
-import "sync"
+import (
+	"sync"
+
+	"github.com/Yiling-J/theine-go"
+)
 
 func Keyed[K comparable, V any](f func(key K) V) func(key K) V {
 	var (
@@ -47,6 +51,31 @@ func KeyedErr[K comparable, V any](f func(key K) (V, error)) func(key K) (V, err
 		}
 
 		m[key] = v
+		return v, nil
+	}
+}
+
+func KeyedErrTheine[K comparable, V any](f func(key K) (V, error), maxSize int64) func(key K) (V, error) {
+	var cache, err = theine.NewBuilder[K, V](maxSize).Build()
+
+	if err != nil {
+		panic("failed to build theine cache")
+	}
+
+	return func(key K) (V, error) {
+		v, ok := cache.Get(key)
+
+		if ok {
+			return v, nil
+		}
+
+		v, err := f(key)
+
+		if err != nil {
+			return v, err
+		}
+
+		cache.Set(key, v, 1)
 		return v, nil
 	}
 }
